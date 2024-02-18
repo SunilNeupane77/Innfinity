1d4e3e646418159c36f39db156087101a16ce318
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-02-19 02:03:20 +0545

    navbar code under ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-
-function Navbar() {
+'use client';
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null); 
   return (
-    <div>Navbar</div>
+    <>
+    </>
   )
 }
 
